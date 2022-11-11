cfc17ccb935d8e79979c13569f22acaa7182210a
Edu Iglesias <eiglesiash@gmail.com>
2022-11-11 12:04:49 +0100

    Incluida la configuración para el cors

--- config/server.js
+++ config/server.js
@@ -6,4 +6,10 @@ module.exports = ({ env }) => ({
       secret: env('ADMIN_JWT_SECRET', '15e1ab4b5c56cea765c2708fe2311773'),
     },
   },
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ['*']
+    },
+  },
 });
